80ce5a222e1ce5f56148b7ffe52281a84e3ed45c
Andrew Pryshchepa <vinglfm@gmail.com>
2016-12-17 15:26:17 +0200

    Added cross origin header

--- app.js
+++ app.js
@@ -10,6 +10,12 @@ var app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:8000');
+  res.header('Access-Control-Allow-Methods', 'GET');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 app.use('/dictionary', dictionary);
 

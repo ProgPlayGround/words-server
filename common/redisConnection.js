15043b5a820c8212360a5823a57cb5494db19a85
Andrew Pryshchepa <vinglfm@gmail.com>
2017-05-14 20:06:54 +0300

    Using docker redis

--- common/redisConnection.js
+++ common/redisConnection.js
@@ -1,4 +1,4 @@
 var redis = require('redis'),
-    redisClient = redis.createClient();
+    redisClient = redis.createClient({'host': process.env.HOST || '127.0.0.1'});
 
  module.exports = redisClient;

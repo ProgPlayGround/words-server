d3f92628be1b48ee92064f53baeea883ba7d7e8d
Andrew Pryshchepa <vinglfm@gmail.com>
2017-05-25 23:04:19 +0300

    Using dictionary for quiz en logic

--- routes/quiz.js
+++ routes/quiz.js
@@ -4,6 +4,27 @@ var supportedLang = config.languages.split(',');
 var mongojs = require('mongojs');
 var db = mongojs(config.dbUrl);
 
+function getOptions(answers, correct) {
+  var options = [correct];
+  var pos = 1;
+  do {
+    var index = Math.floor(Math.random() * answers.length);
+    if(options.indexOf(answers[index]) === -1) {
+      options[pos++] = answers[index];
+    }
+  } while(pos < 4);
+
+  return shuffleOptions(options);
+}
+
+function shuffleOptions(options) {
+  var index = Math.floor(Math.random() * options.length);
+  var temp = options[0];
+  options[0] = options[index];
+  options[index] = temp;
+  return options;
+}
+
 router.get('/:lang', function(req, res, next) {
   if(supportedLang.indexOf(req.params.lang) === -1) {
     return res.status(404).send({
@@ -11,8 +32,25 @@ router.get('/:lang', function(req, res, next) {
       'message': 'Not supported language'
     });
   }
-  db.collection('quiz_' + req.params.lang).find({}, {_id:0}).toArray(function(err, data) {
-    res.send(data);
+  db.collection('dictionary').find({}, {_id:0, audioUrl:0, imageUrl:0}, {limit: 50}, function(err, data) {
+    if(err) {
+      return res.status(500).send({
+        'success': false,
+        'message': err
+      });
+    } else {
+      var answers = data.map(function(elem) {
+        return elem.translation[0];
+      });
+      var quiz = data.map(function(elem) {
+        return {
+          "word": elem.word,
+          "answer": elem.translation[0],
+          "options": getOptions(answers, elem.translation[0])
+        }
+      });
+      return res.send(quiz);
+    }
   });
 });
 

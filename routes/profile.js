5c700717c1d5e3d5d3e01cd2f4c1cc6e64995ee9
Andrew Pryshchepa <vinglfm@gmail.com>
2017-08-13 23:47:50 +0300

    Added logic for user info page

--- routes/profile.js
+++ routes/profile.js
@@ -1,12 +1,94 @@
 var router = require('express').Router();
-var dbUrl = require('../config').dbUrl;
-var ObjectID = require('mongodb').ObjectID;
-var mongojs = require('mongojs');
-var db = mongojs(dbUrl);
+var config = require('../config');
+var db = require('mongojs')(config.dbUrl);
+var auth = require('../common/authorization');
+var storage = require('../common/storage');
 
-router.get('/', function(req, res, next) {
-  db.collection('user').findOne({'_id': ObjectID('58a75c68f46f5e2d3aa9ee77')}, function(err, data) {
-    res.send(data);
+var multer = require('multer');
+var upload = multer({
+  storage: multer.memoryStorage(),
+  limits: {
+    fileSize: config.maxFileSize,
+    files: 1
+  }
+});
+
+var Q = require('q');
+
+router.param('user', auth.validateUser);
+
+function updateProfile(req, res, initial) {
+  Q.fcall(function() {
+    return initial;
+  }).then(function(profile) {
+    if(req.files.length !== 0) {
+      return storage.upload('profile-' + req.params.user, config.s3ImgBucket, req.files[0].buffer)
+        .then(function(response) {
+          profile.avatar = response;
+          return profile;
+        }).catch(function(err) {
+          debug('Error occured while uploading users image, %s', err);
+          return profile;
+        });
+    } else {
+      return profile;
+    }
+  }).then(function(profile) {
+    db.collection('user').update({'_id': req.params.user}, {
+      $set: {
+        'name': req.body.name,
+        'surname': req.body.surname,
+        'birthday': req.body.birthday,
+        'avatar': profile.avatar
+      }
+    }, function(err, result) {
+      if(err) {
+        throw err;
+      } else {
+        return res.status(200).send();
+      }
+    });
+  });
+}
+
+function calculateAge(birthday) {
+   var ageDifMs = Date.now() - birthday.getTime();
+   var ageDate = new Date(ageDifMs);
+   return Math.abs(ageDate.getUTCFullYear() - 1970);
+ }
+
+router.get('/:user', function(req, res, next) {
+  db.collection('user').findOne({'_id': req.params.user}, {'_id': 0, 'category': 0}, function(err, data) {
+    if(err) {
+      return res.status(500).send({
+        'message': err
+      });
+    } else if(data.length === 0) {
+      return res.status(204).send({
+        'message': 'User doesn\'t exist'
+      });
+    } else {
+      if(data.birthday) {
+        data.age = calculateAge(new Date(data.birthday));
+      }
+      return res.send(data);
+    }
+  });
+});
+
+router.put('/:user', upload.any(), function(req, res, next) {
+  db.collection('user').findOne({'_id': req.params.user}, function(err, data) {
+    if(err) {
+      return res.status(500).send({
+        'message': err
+      });
+    } else if(data.length === 0) {
+      return res.status(204).send({
+        'message': 'User doesn\'t exist'
+      });
+    } else {
+      return updateProfile(req, res, data);
+    }
   });
 });
 
